51d5a8598bdeb99c492985902f92404722b79dc1
ByteAtATime <byteatatime@proton.me>
2024-12-23 11:07:14 -0800

    feat: comment out unused navbar on mobile

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -52,7 +52,7 @@ export const Header = () => {
   return (
     <div className="sticky lg:static top-0 navbar bg-base-100 min-h-0 flex-shrink-0 justify-between z-20 px-0 sm:px-2">
       <div className="navbar-start w-auto lg:w-1/2">
-        <div className="lg:hidden dropdown" ref={burgerMenuRef}>
+        {/* <div className="lg:hidden dropdown" ref={burgerMenuRef}>
           <label
             tabIndex={0}
             className={`ml-1 btn btn-ghost ${isDrawerOpen ? "hover:bg-secondary" : "hover:bg-transparent"}`}
@@ -73,7 +73,7 @@ export const Header = () => {
               <HeaderMenuLinks />
             </ul>
           )}
-        </div>
+        </div> */}
         <Link href="/" passHref className="flex items-center gap-2 ml-4 mr-6 shrink-0">
           <span className="font-bold text-xl">Signatorio</span>
         </Link>

e3ba19e506e5e2a75521c45eda6ba44652daf392
ByteAtATime <byteatatime@proton.me>
2024-12-23 10:44:54 -0800

    feat: replace "welcome to scaffold-eth" intro

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -68,9 +68,9 @@ const Home: NextPage = () => {
   return (
     <div className="flex items-center flex-col flex-grow pt-10">
       <div className="px-5 container max-w-screen-sm">
-        <h1 className="text-center">
-          <span className="block text-2xl mb-2">Welcome to</span>
-          <span className="block text-4xl font-bold">Scaffold-ETH 2</span>
+        <h1 className="text-center mb-4">
+          <span className="block text-4xl font-bold">Signator.IO</span>
+          <span className="block text-xl">Request & view signatures</span>
         </h1>
 
         <div className="flex flex-col">
